907a2dbb6027fd9b5efa25ef413829aa90d151b4
StevendeJong🍟 <steven@steven-dejong.nl>
2022-01-13 14:26:31 +0100

    Track error

--- call_it_magic/spotify_functions.py
+++ call_it_magic/spotify_functions.py
@@ -100,9 +100,10 @@ def build_playlist(playlist_id):
 
     for get_id in playlists:
         for track in session['spotify'].playlist(get_id).get('tracks').get('items'):
-            track_id = track.get('track').get('id')
-            if track_id not in build_track_list and track_id not in get_blacklist().get('tracks'):
-                build_track_list.append(track_id)
+	    if track.geet('track'):
+                track_id = track.get('track').get('id')
+                if track_id not in build_track_list and track_id not in get_blacklist().get('tracks'):
+                    build_track_list.append(track_id)
 
     try:
         session['spotify'].playlist_replace_items(playlist_id, [])
